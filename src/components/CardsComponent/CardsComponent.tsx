83b8d65a9e74daa5307bfd647492c77fa9db7a18
vruahali-pawar18 <vrushalipawar1802@gmail.com>
2024-05-20 15:22:20 +0530

    card animations

--- src/components/CardsComponent/CardsComponent.tsx
+++ src/components/CardsComponent/CardsComponent.tsx
@@ -1,8 +1,13 @@
-"use client"
-import React from 'react';
+
+"use client";
+
+import React, { useEffect, useRef } from 'react';
 import { useQuery, gql } from '@apollo/client';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import "../../app/qualitiaCustomers/page.css";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const cardData = gql`
 query GetCarComponentData {
@@ -29,81 +34,95 @@ query GetCarComponentData {
       }
     }
   }    
-`
-
+`;
 
 const CardsComponent = () => {
-    // const CardData = {
-    //     heading: "Identify defects early, continuously improve quality and roll out Salesforce releases faster",
-    //     borderColor: "#76CCFF",
-    //     cards: [
-    //         {
-    //             icons: { src: "/cardImages/card1.png", alt: "card1" },
-    //             description: "The Al-powered test automation generation, just on a single click",
-
-    //         },
-    //         {
-    //             icons: { src: "/cardImages/card2.png", alt: "card2" },
-    //             description: "The Al-powered test automation generation, just on a single click",
-
-    //         },
-    //         {
-    //             icons: { src: "/cardImages/card3.png", alt: "card3" },
-    //             description: "The Al-powered test automation generation, just on a single click",
-
-    //         },
-    //         {
-    //             icons: { src: "/cardImages/card4.png", alt: "card4" },
-    //             description: "The Al-powered test automation generation, just on a single click",
-
-    //         },
-
-    //     ],
-
-    // };
-
     const { loading, error, data } = useQuery(cardData);
+    const containerRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>error</p>
-    console.log("cardData------>", data);
+    useEffect(() => {
+        if (!loading && data) {
+            data.cardsComponents.data.forEach((_: any, index: any) => {
+                const container = containerRefs.current[index];
+                if (container) {
+                    const cards = container.querySelectorAll('.cardComp');
+                    const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
 
-    console.log("cardSectionData=====", data?.cardsComponents?.data[0].attributes?.cardSectiondata);
+                    tl.fromTo(
+                        cards,
+                        { y: 500, opacity: 0 },
+                        { y: 0, opacity: 1, duration: 0.7, ease: "slow(0.7, 0.7, false)", stagger: 0.2 }
+                    );
+
+                    ScrollTrigger.create({
+                        trigger: container,
+                        start: 'top 45%',
+                        end: 'bottom 10%',
+                        animation: tl,
+                        onEnter: () => tl.play(),
+                        onLeaveBack: () => tl.reverse(),
+                        // markers: true,
+                    });
+                }
+            });
+        }
+    }, [loading, data]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+
+    const getGridClasses = (cardCount: number) => {
+        switch (cardCount) {
+            case 1:
+            case 2:
+            case 3:
+                return 'grid-cols-3 gap-20';
+            case 4:
+            default:
+                return 'grid-cols-4';
+        }
+    };
 
     return (
         <>
             {data?.cardsComponents?.data?.map((cardComponent: any, index1: any) => {
                 const cardCount = cardComponent.attributes?.cardSectiondata?.length;
-                const gridColumnClass = `grid-cols-${Math.min(cardCount, 4)}`;
-                const gapValue = cardCount === 3 ? 20 : 0;
-                console.log(gridColumnClass,'66666');
-                
+                const gridColumnClass = getGridClasses(cardCount);
+
                 return (
-                    <div key={index1} className='max-w-6xl mx-auto flex flex-col justify-center items-center'>
-                        <div className='container px-5 py-24 max-w-6xl mx-auto flex items-center justify-center pl-10'>
-                            <h1 className="title-font md:text-2xl lg:text-4xl mb-4 w-4/5 inline-block gap- font-bold text-[#032d60]">{cardComponent.attributes.heading}</h1>
+                    <div
+                        key={index1}
+                        ref={(el) => { containerRefs.current[index1] = el; }}
+                        className='max-w-6xl mx-auto flex flex-col justify-center items-center'
+                    >
+                        <div className='px-5 py-24 max-w-6xl mx-auto flex items-center justify-center pl-10'>
+                            <h1 className="title-font md:text-2xl lg:text-4xl mb-4 w-4/5 inline-block font-bold text-[#032d60]">
+                                {cardComponent.attributes.heading}
+                            </h1>
                         </div>
-                        <div className={`flex flex-col md:grid ${gridColumnClass} pb-16 md:pb-12 md:gap-${gapValue}`}>
-                            {cardComponent.attributes.cardSectiondata?.map((card: any, index: any) => {
-                                return (
-                                    <div key={index} className={`gap-4 fadeInDown h-96 w-11/12 rounded-2xl flex flex-col items-center justify-center pb-5 border-t-[15px]`}
-                                        style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px', borderColor : cardComponent.attributes.topBorderColor }}
-                                    >
-                                            <img src={card.cardIcons.data.attributes.name} />
-                                            <h1 className={`${cardCount === 4 ? "w-4/5" : "w-3/5"} text-[#204572] text-lg pt-3 font-medium  text-center justify-center`}>{card.cardDescription}</h1>
-                                    </div>
-                                );
-                            })}
+                        <div className={`flex flex-col md:grid ${gridColumnClass} pb-16 md:pb-12 gap-5`}>
+                            {cardComponent.attributes.cardSectiondata?.map((card: any, index: any) => (
+                                <div
+                                    key={index}
+                                    className="cardComp gap-4 h-96 w-11/12 rounded-2xl flex flex-col items-center justify-center pb-5 border-t-[15px]"
+                                    style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px', borderColor: cardComponent.attributes.topBorderColor }}
+                                >
+                                    <img src={card.cardIcons.data.attributes.name} alt={card.cardIcons.data.attributes.alternativeText} />
+                                    <h1 className={`${cardCount === 4 ? "w-4/5" : "w-3/5"} text-[#204572] text-lg pt-3 font-medium text-center justify-center`}>
+                                        {card.cardDescription}
+                                    </h1>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 );
-            })
-            }
-
+            })}
         </>
-
     );
-}
-
+};
 
 export default CardsComponent;
+
+
+
+
--- src/components/QualitiaCustomers/QualitiaCustomers.tsx
+++ src/components/QualitiaCustomers/QualitiaCustomers.tsx
@@ -1,9 +1,10 @@
-
 // "use client"
+// import React, { useEffect, useRef } from 'react';
 // import { gql, useQuery } from '@apollo/client';
-// import React, { useEffect } from 'react';
-// import "../../app/qualitiaCustomers/page.css";
-// import "../../app/qualitiaCustomers/customerAnimation";
+// import gsap from 'gsap'; // Import GSAP
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// gsap.registerPlugin(ScrollTrigger);
 
 
 // const customersData = gql`
@@ -25,50 +26,188 @@
 //       }
 //     }
 //   }
-// `
+// `;
 
 // const QualitiaCustomers = () => {
-
 //     const { loading, error, data } = useQuery(customersData);
+//     const containerRef = useRef<HTMLDivElement>(null);
 
-//     if (loading) return <p>Loading...</p>
-//     if (error) return <p>error</p>
+//     useEffect(() => {
+//         if (!loading && data && containerRef.current && data.qualitiaCustomers.data) {
+//             const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+//             const leftHalfElements = containerRef.current.querySelectorAll('.left-half');
+//             const rightHalfElements = containerRef.current.querySelectorAll('.right-half');
+//             const scroll = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
 
-//     console.log(data);
+//             tl.fromTo(
+//                 leftHalfElements,
+//                 { x: '-100%', opacity: 0 },
+//                 { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 }
+//             ).fromTo(
+//                 rightHalfElements,
+//                 { x: '100%', opacity: 0 },
+//                 { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 },
+//             );
+
+
+//             // Attach ScrollTrigger to the timeline
+//             ScrollTrigger.create({
+//                 trigger: containerRef.current,
+//                 start: 'top 70%',
+//                 end: " bottom 10%",
+//                 onEnter: () => tl.play(),
+//                 onEnterBack: () => tl.reverse(),
+//                 animation: tl
+//             });
+//         }
+//     }, [loading, data]);
+
+
+//     if (loading) return <p>Loading...</p>;
+//     if (error) return <p>Error</p>;
+
+//     console.log("QualitiaCustomers=======", data)
 
 //     return (
-//         <div className='max-w-6xl mx-auto pt-28'>
+//         <div className='max-w-6xl mx-auto pt-28' ref={containerRef}>
 //             <div className='flex items-center justify-center'>
 //                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-sky-900">Qualitia Customers</h1>
 //             </div>
-//             <div className=" container grid sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-5 pb-2 ">
-//                 {data.qualitiaCustomers.data[0].attributes?.image.data.map((customer: any, index: React.Key | null | undefined) => (
-//                     <div className='rounded-md fadeInLeft flex justify-center items-center w-8/12 sm:w-full mx-auto' key={index}
-//                         style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
-//                     >
-//                         <img key={index} className="h-20 w-24 object-contain" src={customer.attributes.name} alt={customer.attributes.alternativeText} />
-//                     </div>
-//                 ))}
-
+//             <div className="grid sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-5 pb-2 ">
+//                 {data.qualitiaCustomers.data[0].attributes?.image.data.map((customer: any, index: any) => {
+//                     const getClassForIndex = (leftIndex : number) => {
+//                         const indexes = [0, 1, 2, 6, 7, 8, 12, 13, 14];
+//                         const indexInSequence = indexes.indexOf(leftIndex);
+//                         return indexInSequence !== -1 || indexInSequence % 3 === 0 ? 'left-half' : 'right-half';
+//                     };
+//                     return (
+//                         <div
+//                             className={`rounded-md flex justify-center items-center w-8/12 sm:w-full mx-auto ${getClassForIndex(index)}`}
+//                             key={index}
+//                             style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
+//                         >
+//                             <img className="h-20 w-24 object-contain" src={customer.attributes.name} alt={customer.attributes.alternativeText} />
+//                         </div>
+//                     );
+//                 })}
 //             </div>
+
 //             <div className="flex flex-col justify-center items-center text-xl pt-16 pb-4">
 //                 <button className='border border-[#032d60] rounded-full h-12 w-36 text-lg text-[#032d60] font-bold'>See more</button>
 //             </div>
 //         </div>
-
 //     );
 // };
 
 // export default QualitiaCustomers;
 
-"use client"
+
+
+
+
+// "use client";
+// import React, { useEffect, useRef } from 'react';
+// import { gql, useQuery } from '@apollo/client';
+// import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// const customersData = gql`
+// query GetQualitiaCustomers {
+//     qualitiaCustomers {
+//       data {
+//         id
+//         attributes {
+//           image(pagination:{pageSize:100}){
+//             data {
+//               id
+//               attributes {
+//                 name
+//                 alternativeText
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+// const QualitiaCustomers = () => {
+//     const { loading, error, data } = useQuery(customersData);
+//     const containerRef = useRef<HTMLDivElement>(null);
+
+//     useEffect(() => {
+//         if (!loading && data && containerRef.current) {
+//             const leftHalfElements = containerRef.current.querySelectorAll('.left-half');
+//             const rightHalfElements = containerRef.current.querySelectorAll('.right-half');
+
+//             const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+
+//             tl.fromTo(
+//                 leftHalfElements,
+//                 { x: '-100%', opacity: 0 },
+//                 { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 }
+//             ).fromTo(
+//                 rightHalfElements,
+//                 { x: '100%', opacity: 0 },
+//                 { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 },
+//                 "-=0.7" 
+//             );
+
+//             ScrollTrigger.create({
+//                 trigger: containerRef.current,
+//                 start: 'top 55%',
+//                 end: 'bottom 10%',
+//                 animation: tl,
+//                 onEnter: () => tl.play(),
+//                 onLeaveBack: () => tl.reverse(),
+//             });
+//         }
+//     }, [loading, data]);
+
+//     if (loading) return <p>Loading...</p>;
+//     if (error) return <p>Error</p>;
+
+//     const getClassForIndex = (index: number) => {
+//         const leftHalfIndexes = [0, 1, 2, 6, 7, 8, 12, 13, 14];
+//         return leftHalfIndexes.includes(index) ? 'left-half' : 'right-half';
+//     };
+
+//     return (
+//         <div className='max-w-6xl mx-auto pt-28' ref={containerRef}>
+//             <div className='flex items-center justify-center'>
+//                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-sky-900">Qualitia Customers</h1>
+//             </div>
+//             <div className="grid sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-5 pb-2">
+//                 {data.qualitiaCustomers.data[0].attributes?.image.data.map((customer: any, index: any) => (
+//                     <div
+//                         className={`rounded-md flex justify-center items-center w-8/12 sm:w-full mx-auto ${getClassForIndex(index)}`}
+//                         key={customer.id}
+//                         style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
+//                     >
+//                         <img className="h-20 w-24 object-contain" src={customer.attributes.name} alt={customer.attributes.alternativeText} />
+//                     </div>
+//                 ))}
+//             </div>
+//             <div className="flex flex-col justify-center items-center text-xl pt-16 pb-4">
+//                 <button className='border border-[#032d60] rounded-full h-12 w-36 text-lg text-[#032d60] font-bold'>See more</button>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default QualitiaCustomers;
+
+
+"use client";
 import React, { useEffect, useRef } from 'react';
 import { gql, useQuery } from '@apollo/client';
-import gsap from 'gsap'; // Import GSAP
+import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
-
 
 const customersData = gql`
 query GetQualitiaCustomers {
@@ -96,66 +235,59 @@ const QualitiaCustomers = () => {
     const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        if (!loading && data && containerRef.current && data.qualitiaCustomers.data) {
-            const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+        if (!loading && data && containerRef.current) {
             const leftHalfElements = containerRef.current.querySelectorAll('.left-half');
             const rightHalfElements = containerRef.current.querySelectorAll('.right-half');
-            const scroll = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+
+            const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
 
             tl.fromTo(
                 leftHalfElements,
                 { x: '-100%', opacity: 0 },
-                { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 }
+                { x: '0%', opacity: 1, duration: 1.1, ease : "slow(0.7, 0.7, false)"}
             ).fromTo(
                 rightHalfElements,
                 { x: '100%', opacity: 0 },
-                { x: '0%', opacity: 1, stagger: 0.05, duration: 0.7 },
+                { x: '0%', opacity: 1, duration: 1.1, ease : "slow(0.7, 0.7, false)"},
+                "-=1.1" // Start the right elements animation at the same time as left elements animation
             );
 
-
-            // Attach ScrollTrigger to the timeline
             ScrollTrigger.create({
                 trigger: containerRef.current,
                 start: 'top 70%',
-                end: " bottom 10%",
+                end: 'bottom 10%',
+                animation: tl,
                 onEnter: () => tl.play(),
-                onEnterBack: () => tl.reverse(),
-            
-                animation: tl
+                onLeaveBack: () => tl.reverse(),
             });
         }
     }, [loading, data]);
 
-
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error</p>;
 
-    console.log("QualitiaCustomers=======", data)
+    const isLeftHalfIndex = (index:number) => {
+        // const group = Math.floor(index / 6);
+        const positionInGroup = index % 6;
+        return positionInGroup < 3;
+    };
 
     return (
         <div className='max-w-6xl mx-auto pt-28' ref={containerRef}>
             <div className='flex items-center justify-center'>
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-sky-900">Qualitia Customers</h1>
             </div>
-            <div className="grid sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-5 pb-2 ">
-                {data.qualitiaCustomers.data[0].attributes?.image.data.map((customer: any, index: any) => {
-                    const getClassForIndex = (leftIndex : number) => {
-                        const indexes = [0, 1, 2, 6, 7, 8, 12, 13, 14];
-                        const indexInSequence = indexes.indexOf(leftIndex);
-                        return indexInSequence !== -1 || indexInSequence % 3 === 0 ? 'left-half' : 'right-half';
-                    };
-                    return (
-                        <div
-                            className={`rounded-md flex justify-center items-center w-8/12 sm:w-full mx-auto ${getClassForIndex(index)}`}
-                            key={index}
-                            style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
-                        >
-                            <img className="h-20 w-24 object-contain" src={customer.attributes.name} alt={customer.attributes.alternativeText} />
-                        </div>
-                    );
-                })}
+            <div className="grid sm:grid-cols-4 lg:grid-cols-6 gap-3 pt-5 pb-2">
+                {data.qualitiaCustomers.data[0].attributes?.image.data.map((customer: any, index: any) => (
+                    <div
+                        className={`rounded-md flex justify-center items-center w-8/12 sm:w-full mx-auto ${isLeftHalfIndex(index) ? 'left-half' : 'right-half'}`}
+                        key={customer.id}
+                        style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
+                    >
+                        <img className="h-20 w-24 object-contain" src={customer.attributes.name} alt={customer.attributes.alternativeText} />
+                    </div>
+                ))}
             </div>
-
             <div className="flex flex-col justify-center items-center text-xl pt-16 pb-4">
                 <button className='border border-[#032d60] rounded-full h-12 w-36 text-lg text-[#032d60] font-bold'>See more</button>
             </div>
@@ -164,5 +296,3 @@ const QualitiaCustomers = () => {
 };
 
 export default QualitiaCustomers;
-
-
